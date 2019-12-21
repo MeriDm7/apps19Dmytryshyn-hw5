081f9ddea021d6dcd63d2ed88ba9f018b6260b96
MeriDm7 <dmytryshyn.m@ucu.edu.ua>
2019-12-21 20:46:33 +0200

    add tests

--- src/test/java/ua/edu/ucu/AsIntStreamTest.java
+++ src/test/java/ua/edu/ucu/AsIntStreamTest.java
@@ -5,20 +5,18 @@ import org.junit.Test;
 import ua.edu.ucu.stream.AsIntStream;
 import ua.edu.ucu.stream.IntStream;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 public class AsIntStreamTest {
 
     private IntStream intStream;
 
-    @Before
-    public void init() {
-        int[] intArr = {-1, 0, 1, 2, 3};
-        intStream = AsIntStream.of(intArr);
-    }
 
     @Test
     public void testAverage() {
+        int[] intArr = {-1, 0, 1, 2, 3};
+        intStream = AsIntStream.of(intArr);
         double expResult = 1.0;
         double result = intStream.average();
         assertEquals(expResult, result, 0.001);
@@ -26,6 +24,8 @@ public class AsIntStreamTest {
 
     @Test
     public void testMax() {
+        int[] intArr = {-1, 0, 1, 2, 3};
+        intStream = AsIntStream.of(intArr);
         int expResult = 3;
         int result = intStream.max();
         assertEquals(expResult, result);
@@ -33,8 +33,73 @@ public class AsIntStreamTest {
 
     @Test
     public void testMin() {
+        int[] intArr = {-1, 0, 1, 2, 3};
+        intStream = AsIntStream.of(intArr);
         int expResult = -1;
         int result = intStream.min();
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void testCount() {
+        int[] intArr = {-1, 0, 1, 2, 3};
+        intStream = AsIntStream.of(intArr);
+        long expResult = 5;
+        long result = intStream.count();
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void testSum() {
+        int[] intArr = {-1, 0, 1, 2, 3};
+        intStream = AsIntStream.of(intArr);
+        int expResult = 5;
+        int result = intStream.sum();
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void testToArray() {
+        int[] intArr = {-1, 0, 1, 2, 3};
+        intStream = AsIntStream.of(intArr);
+        int[] expResult = {-1, 0, 1, 2, 3};
+        int[] result = intStream.toArray();
+        assertArrayEquals(expResult, result);
+    }
+
+    @Test
+    public void testFilter() {
+        int[] intArr = {-1, 0, 1, 2, 3};
+        intStream = AsIntStream.of(intArr);
+        int[] expResult = {1, 2, 3};
+        int[] result = intStream.filter(x -> x > 0).toArray();
+        assertArrayEquals(expResult, result);
+    }
+
+    @Test
+    public void testMap() {
+        int[] intArr = {-1, 0, 1, 2, 3};
+        intStream = AsIntStream.of(intArr);
+        int[] expResult = {1, 0, 1, 4, 9};
+        int[] result = intStream.map(x -> x * x).toArray();
+        assertArrayEquals(expResult, result);
+    }
+
+    @Test
+    public void testFlatMap() {
+        int[] intArr = {-1, 0, 1, 2, 3};
+        intStream = AsIntStream.of(intArr);
+        int[] expResult = {-2, -1, 0, -1, 0, 1, 0, 1, 2, 1, 2, 3, 2, 3, 4};
+        int[] result = intStream.flatMap(x -> AsIntStream.of(x - 1, x, x + 1)).toArray();
+        assertArrayEquals(expResult, result);
+    }
+
+    @Test
+    public void testReduce() {
+        int[] intArr = {-1, 0, 1, 2, 3};
+        intStream = AsIntStream.of(intArr);
+        int expResult = 10;
+        int result = intStream.reduce(5, (sum, x) -> sum += x);
         assertEquals(expResult, result);
     }
 }
